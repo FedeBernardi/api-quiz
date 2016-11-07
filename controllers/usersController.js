375cdb77989b4b76030ac4f37fc767d4f043093d
BERNF018 <Federico.X.Bernardi.-ND@disney.com>
2016-11-07 15:10:45 -0300

    AutoLogin functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,9 +2,9 @@ var jwt = require('jsonwebtoken');
 var User = require('../models/users');
 var config = require('../config');
 
-function addUser(req, res){
+function addUser(req, res) {
   var user = req.body;
-  User.addUser(user, function(err, data){
+  User.addUser(user, (err, data) => {
     if(err){
       throw err;
     }
@@ -13,9 +13,9 @@ function addUser(req, res){
 }
 
 //All users are returned except the current user.
-function getAllUsers(req, res){
+function getAllUsers(req, res) {
   var currentUser = new RegExp(req.body.username);
-  User.getUser(currentUser, function(err, data){
+  User.getUser(currentUser, (err, data) => {
     if(err){
       throw err;
     }
@@ -23,22 +23,22 @@ function getAllUsers(req, res){
   })
 }
 
-function loginUser(req, res){
+function loginUser(req, res) {
   var creds = req.body;
-  User.loginUser(creds, function(err, data){
+
+  User.loginUser(creds, (err, data) => {
     if(err){
       throw err;
     }
 
     if(data.length == 0) {
+      res.status(204);
       sendData = {
         error: 'Incorrect username or password'
       };
-      res.status(204);
     } else {
       var username = data[0].username;
       var token = jwt.sign({username}, config.secret, {expiresIn: '10h'});
-
       sendData = {
         username,
         token
@@ -49,8 +49,56 @@ function loginUser(req, res){
   })
 }
 
+function autoLogin(req, res) {
+  var authHeader = req.headers.authorization,
+      token = authHeader.split(" ")[1],
+      username = getUsernameFromDecodedToken(token),
+      sendData;
+
+  User.autoLogin(username, (err, data) => {
+    if(err){
+      res.status(401);
+      throw err;
+    }
+
+    if (data.length == 0) {
+      res.status(204);
+      sendData = {
+        error: 'Incorrect username'
+      };
+    } else {
+      sendData = data[0];
+    }
+
+    res.json(sendData);
+  })
+}
+
+// Decodes the given token.
+//
+// @parameters: String token
+// @return: String
+function decodeJwt (token) {
+    var base64Url = token.split('.')[1],
+        base64 = base64Url.replace('-', '+').replace('_', '/'),
+        buffer = Buffer(base64, 'base64'),
+        json = JSON.parse(buffer);
+
+    return json;
+};
+
+// Gets the username from the payload of the token.
+//
+// @parameters: String token
+function getUsernameFromDecodedToken(token) {
+  var json = decodeJwt(token);
+
+  return json.username;
+}
+
 module.exports = {
   getAllUsers,
   addUser,
-  loginUser
+  loginUser,
+  autoLogin
 }
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ var config = require('../config');
 var usersRouter = express.Router();
 var usersController = require('../controllers/usersController');
 
-usersRouter.post('/all',usersController.getAllUsers);
-usersRouter.post('/login',usersController.loginUser);
-usersRouter.post('/registration',usersController.addUser);
+usersRouter.post('/all', usersController.getAllUsers);
+usersRouter.post('/login', usersController.loginUser);
+usersRouter.post('/registration', usersController.addUser);
+usersRouter.post('/auto-login', usersController.autoLogin);
 
 module.exports = {
   usersRouter
